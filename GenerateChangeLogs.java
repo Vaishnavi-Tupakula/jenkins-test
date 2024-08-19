91700bfb71e048e681638dca1b8b84b326a1972c
Vaishnavi-Tupakula <tupakula.vaishnavi.work@gmail.com>
2024-08-19 17:28:10 +0530

    Added GenerateChangeLog.java file

---
+++ GenerateChangeLogs.java
@@ -0,0 +1,122 @@
+import javax.xml.parsers.DocumentBuilder; 
+import javax.xml.parsers.DocumentBuilderFactory; 
+import javax.xml.transform.Transformer; 
+import javax.xml.transform.TransformerFactory; 
+import javax.xml.transform.dom.DOMSource; 
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.OutputKeys;
+import org.w3c.dom.Document; 
+import org.w3c.dom.Element;
+import java.io.File;
+import java.util.*;
+
+public class GenerateChangeLogs {
+
+    private static final String rootfolder="D:\\Projects\\jenkins-test\\Release 1.0.0";
+
+    private static String curFolder="";
+
+    //Generate XML
+    public static Element generateXML() throws Exception
+    {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance(); 
+        DocumentBuilder builder = factory.newDocumentBuilder(); 
+  
+        // Create a new Document 
+        Document document = builder.newDocument(); 
+  
+        // Create root element 
+        Element root = document.createElement("databaseChangeLog");
+        root.setAttribute("xmlns","http://www.liquibase.org/xml/ns/dbchangelog");
+        root.setAttribute("xmlns:xsi","http://www.w3.org/2001/XMLSchema-instance");
+        root.setAttribute("xmlns:ext","http://www.liquibase.org/xml/ns/dbchangelog-ext");
+        root.setAttribute("xmlns:pro","http://www.liquibase.org/xml/ns/pro");
+        root.setAttribute("xsi:schemaLocation","http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-latest.xsd http://www.liquibase.org/xml/ns/dbchangelog-ext http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-ext.xsd http://www.liquibase.org/xml/ns/pro http://www.liquibase.org/xml/ns/pro/liquibase-pro-latest.xsd");
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance(); 
+        Transformer transformer = transformerFactory.newTransformer(); 
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+        DOMSource source = new DOMSource();
+        StreamResult result = new StreamResult("D:\\Projects\\jenkins-test\\changelog.xml"); 
+        transformer.transform(source, result);  
+        return root;
+    }
+
+    public static void check(File[] files) throws Exception
+    {
+        ArrayList<String> sqlfiles=new ArrayList<>();
+        ArrayList<String> xmlfiles=new ArrayList<>();
+
+        for (File filename : files) 
+        {
+            
+            if (filename.isDirectory()) 
+            {
+                findFiles(filename,rootfolder);
+            }
+
+            else if(filename.getName().endsWith(".sql"))
+            {
+                sqlfiles.add(filename.getAbsolutePath());
+            }
+
+            else if(filename.getName().endsWith(".xml"))
+            {
+                xmlfiles.add(filename.getAbsolutePath());
+            }
+        }
+        generateChangeSets(sqlfiles,);
+        generateIncludeTag(xmlfiles);
+    }
+
+    public static void generateChangeSets(ArrayList<String> sqlfiles) throws Exception
+    {
+        if(sqlfiles.size()==0)
+            return;
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance(); 
+        DocumentBuilder builder = factory.newDocumentBuilder(); 
+      
+        // Create a new Document 
+        Document document = builder.newDocument(); 
+      
+        // Create root element 
+        Element root = document.createElement("databaseChangeLog");
+        root.setAttribute("xmlns","http://www.liquibase.org/xml/ns/dbchangelog");
+        root.setAttribute("xmlns:xsi","http://www.w3.org/2001/XMLSchema-instance");
+        root.setAttribute("xmlns:ext","http://www.liquibase.org/xml/ns/dbchangelog-ext");
+        root.setAttribute("xmlns:pro","http://www.liquibase.org/xml/ns/pro");
+        root.setAttribute("xsi:schemaLocation","http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-latest.xsd http://www.liquibase.org/xml/ns/dbchangelog-ext http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-ext.xsd http://www.liquibase.org/xml/ns/pro http://www.liquibase.org/xml/ns/pro/liquibase-pro-latest.xsd");
+    
+        for()
+
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance(); 
+        Transformer transformer = transformerFactory.newTransformer(); 
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+        DOMSource source = new DOMSource();
+        StreamResult result = new StreamResult("D:\\Projects\\jenkins-test\\changelog.xml"); 
+        transformer.transform(source, result); 
+
+    }
+
+    public static void generateIncludeTag(ArrayList<String> xmlfiles)
+    {
+        if(xmlfiles.size()==0)
+            return;
+    }
+
+
+    public static void findFiles(File curFolder,String rootFolder) throws Exception
+    {
+        File[] files=curFolder.listFiles();
+        check(files);
+    }
+
+    public static void main(String[] args) throws Exception {
+        findFiles(new File(rootfolder),rootfolder);
+    }
+    
+}
