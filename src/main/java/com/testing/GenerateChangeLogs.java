ac350ba0bb9362b1c26e7437ad95f6fb8cc97a18
Vaishnavi-Tupakula <tupakula.vaishnavi.work@gmail.com>
2024-10-10 15:55:41 +0530

    Already generated

--- src/main/java/com/testing/GenerateChangeLogs.java
+++ src/main/java/com/testing/GenerateChangeLogs.java
@@ -1,4 +1,5 @@
 package com.testing;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.Transformer;
@@ -15,129 +16,113 @@ import java.util.List;
 
 public class GenerateChangeLogs {
 
-    private static final String ROOT_FOLDER = "src\\main\\resources\\Release 1.0.0";
-    private static final String rootfolder="src\\main\\resources\\Release 1.0.0";
+    private static final String ROOT_FOLDER = System.getenv("WORKSPACE") != null ? System.getenv("WORKSPACE") + "/src/main/resources/Release 1.0.0" 
+                                                                                 : "src/main/resources/Release 1.0.0";
     private static final String MASTER_CHANGELOG_FILE = "master-changelog.xml";
     private static final String TEAM_CHANGELOG_FILE = "team-changelog.xml";
 
-    private static String curFolder="";
-    static int id=1;
-    static int fileid=1;
+    private static String curFolder = "";
+    static int id = 1;
+    static int fileid = 1;
 
-    public static void checksql(File[] files) throws Exception
-    {
-        ArrayList<String> sqlfiles=new ArrayList<>();
+    public static void checksql(File[] files) throws Exception {
+        ArrayList<String> sqlfiles = new ArrayList<>();
 
-        for (File filename : files) 
-        {
-            
-            if (filename.isDirectory()) 
-            {
-                curFolder=filename.getAbsolutePath();
-                findsqlFiles(filename,rootfolder);
-            }
-
-            else if(filename.getName().endsWith(".sql"))
-            {
+        for (File filename : files) {
+            if (filename.isDirectory()) {
+                curFolder = filename.getAbsolutePath();
+                findsqlFiles(filename, ROOT_FOLDER);
+            } else if (filename.getName().endsWith(".sql")) {
                 sqlfiles.add(filename.getAbsolutePath());
-            }
-
-            else if(filename.getName().endsWith(".xml"))
-            {
+            } else if (filename.getName().endsWith(".xml")) {
                 continue;
             }
         }
-        generateChangeSets(sqlfiles,curFolder);
+        generateChangeSets(sqlfiles, curFolder);
     }
 
-    public static void generateChangeSets(ArrayList<String> sqlfiles,String curFolder) throws Exception
-    {
-        if(sqlfiles.size()==0)
+    public static void generateChangeSets(ArrayList<String> sqlfiles, String curFolder) throws Exception {
+        if (sqlfiles.size() == 0)
             return;
 
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance(); 
-        DocumentBuilder builder = factory.newDocumentBuilder(); 
-      
-        // Create a new Document 
-        Document document = builder.newDocument(); 
-      
-        // Create root element 
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        // Create a new Document
+        Document document = builder.newDocument();
+
+        // Create root element
         Element root = document.createElement("databaseChangeLog");
-        root.setAttribute("xmlns","http://www.liquibase.org/xml/ns/dbchangelog");
-        root.setAttribute("xmlns:xsi","http://www.w3.org/2001/XMLSchema-instance");
-        root.setAttribute("xmlns:ext","http://www.liquibase.org/xml/ns/dbchangelog-ext");
-        root.setAttribute("xmlns:pro","http://www.liquibase.org/xml/ns/pro");
-        root.setAttribute("xsi:schemaLocation","http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-latest.xsd http://www.liquibase.org/xml/ns/dbchangelog-ext http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-ext.xsd http://www.liquibase.org/xml/ns/pro http://www.liquibase.org/xml/ns/pro/liquibase-pro-latest.xsd");
-    
+        root.setAttribute("xmlns", "http://www.liquibase.org/xml/ns/dbchangelog");
+        root.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+        root.setAttribute("xmlns:ext", "http://www.liquibase.org/xml/ns/dbchangelog-ext");
+        root.setAttribute("xmlns:pro", "http://www.liquibase.org/xml/ns/pro");
+        root.setAttribute("xsi:schemaLocation",
+                "http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-latest.xsd http://www.liquibase.org/xml/ns/dbchangelog-ext http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-ext.xsd http://www.liquibase.org/xml/ns/pro http://www.liquibase.org/xml/ns/pro/liquibase-pro-latest.xsd");
+
         document.appendChild(root);
-        
-        for(String apath:sqlfiles)
-        {
-            Element ch=document.createElement("changeSet");
-            ch.setAttribute("id",Integer.valueOf(id).toString());
+
+        for (String apath : sqlfiles) {
+            Element ch = document.createElement("changeSet");
+            ch.setAttribute("id", Integer.valueOf(id).toString());
             id++;
-            ch.setAttribute("author","user");
+            ch.setAttribute("author", "user");
 
             root.appendChild(ch);
 
-            Element sqlFile=document.createElement("sqlFile");
-            sqlFile.setAttribute("path", getRelativePath(apath,curFolder));
+            Element sqlFile = document.createElement("sqlFile");
+            sqlFile.setAttribute("path", getRelativePath(apath, curFolder));
 
             ch.appendChild(sqlFile);
-
         }
 
-        TransformerFactory transformerFactory = TransformerFactory.newInstance(); 
-        Transformer transformer = transformerFactory.newTransformer(); 
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
         transformer.setOutputProperty(OutputKeys.INDENT, "yes");
         transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
         DOMSource source = new DOMSource(document);
-        String path=curFolder+"\\"+fileid+"changelog.xml";
-        StreamResult result = new StreamResult(path);
+        String path = curFolder + File.separator + fileid + "changelog.xml";
+        StreamResult result = new StreamResult(new File(path));
         fileid++;
-        transformer.transform(source, result); 
-
+        transformer.transform(source, result);
     }
 
-    public static void findsqlFiles(File curFolder,String rootFolder) throws Exception
-    {
-        File[] files=curFolder.listFiles();
+    public static void findsqlFiles(File curFolder, String rootFolder) throws Exception {
+        File[] files = curFolder.listFiles();
         checksql(files);
     }
 
-    public static String getRelativePath(String fpath,String curFolder)
-    {
-        String res="";
+    public static String getRelativePath(String fpath, String curFolder) {
+        String res = "";
 
-        for(int i=0;i<fpath.length();i++)
-        {
-            if(i>curFolder.length())
-                res+=fpath.charAt(i);
+        for (int i = 0; i < fpath.length(); i++) {
+            if (i > curFolder.length())
+                res += fpath.charAt(i);
         }
 
         return res;
     }
 
-
     public static void main(String[] args) throws Exception {
-
         // Uncomment the following line to delete all generated XML files
-        deleteGeneratedFiles(new File(ROOT_FOLDER));
+         deleteGeneratedFiles(new File(ROOT_FOLDER));
 
-        //findsqlFiles(new File(rootfolder),rootfolder);
+        findsqlFiles(new File(ROOT_FOLDER), ROOT_FOLDER);
 
         // Generate team changelogs
-        //generateTeamChangelogs(new File(ROOT_FOLDER));
-        
+        generateTeamChangelogs(new File(ROOT_FOLDER));
+
         // Generate master changelog
-        //generateMasterChangelog(new File(ROOT_FOLDER));
-        
-        
+        generateMasterChangelog(new File(ROOT_FOLDER));
     }
+
+    // Additional methods unchanged...
+
 
     private static void generateTeamChangelogs(File rootFolder) throws Exception {
         File[] files = rootFolder.listFiles();
-        if (files == null) return;
+        if (files == null)
+            return;
 
         for (File file : files) {
             if (file.isDirectory()) {
@@ -150,15 +135,15 @@ public class GenerateChangeLogs {
     private static void generateTeamChangelog(File teamFolder) throws Exception {
         List<String> sqlFiles = new ArrayList<>();
         List<String> xmlFiles = new ArrayList<>();
-        
+
         // Collect SQL and XML files
         collectFiles(teamFolder, sqlFiles, xmlFiles);
-        
+
         // Generate team changelog if SQL files are found
         if (!sqlFiles.isEmpty()) {
             generateChangeSets(sqlFiles, teamFolder);
         }
-        
+
         // Generate include tags in team changelog if XML files are found
         if (!xmlFiles.isEmpty()) {
             generateIncludeTag(xmlFiles, teamFolder);
@@ -167,7 +152,8 @@ public class GenerateChangeLogs {
 
     private static void collectFiles(File folder, List<String> sqlFiles, List<String> xmlFiles) {
         File[] files = folder.listFiles();
-        if (files == null) return;
+        if (files == null)
+            return;
 
         for (File file : files) {
             if (file.isDirectory()) {
@@ -252,9 +238,10 @@ public class GenerateChangeLogs {
 
     private static void generateMasterChangelog(File rootFolder) throws Exception {
         List<String> teamChangelogs = new ArrayList<>();
-        
+
         File[] files = rootFolder.listFiles();
-        if (files == null) return;
+        if (files == null)
+            return;
 
         for (File file : files) {
             if (file.isDirectory()) {
@@ -299,7 +286,8 @@ public class GenerateChangeLogs {
 
     private static void deleteGeneratedFiles(File folder) {
         File[] files = folder.listFiles();
-        if (files == null) return;
+        if (files == null)
+            return;
 
         for (File file : files) {
             if (file.isDirectory()) {
